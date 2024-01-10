a9f13e66056a4f4b5733ca988994131b36d17ead
Joffre Veloz <joffreandres11@gmail.com>
2024-01-10 18:24:34 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,33 +3,42 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { SendHorizonal } from 'lucide-react'
 import Image from 'next/image'
- 
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="mb-8">
+        <Image
+          src="/ups-logo.png"
+          width={400}
+          height={400}
+          alt="ups-logo"
+        />
+      </div>
       
-      <div className='space-y-3 '>
-      <Image
-      src="/ups-logo.png"
-      width={500}
-      height={500}
-      alt="ups-logo"
-    />
-      <div>ANÁLISIS DE TÉCNICAS Y ESTRATEGIAS DE SEGURIDADES IOT EN DISPOSITIVOS PARA PROTEGER LOS DATOS TRANSMITIDO</div>
-      <div className="grid w-full max-w-sm items-center gap-1.5 mx-auto">
+      <div className="text-center">
+        <h1 className="text-2xl font-bold mb-4">
+          ANÁLISIS DE TÉCNICAS Y ESTRATEGIAS DE SEGURIDAD EN DISPOSITIVOS IOT PARA PROTEGER LOS DATOS TRANSMITIDOS
+        </h1>
+      </div>
+      
+      <div className="grid w-full max-w-sm items-center gap-1.5">
         <Label htmlFor="email">Correo</Label>
         <Input type="email" id="email" placeholder="Email" />
       </div>
+      
       <div className="grid w-full max-w-sm items-center gap-1.5">
         <Label htmlFor="password">Contraseña</Label>
         <Input type="password" id="password" placeholder="Contraseña" />
       </div>
-      <Button className='w-full'>
-      Ingresar
-      <SendHorizonal className="ml-2 h-4 w-4" />
-    </Button>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Button className="w-full mt-4">
+        Ingresar
+        <SendHorizonal className="ml-2 h-4 w-4" />
+      </Button>
       </div>
+      
+      
     </div>
   )
 }
