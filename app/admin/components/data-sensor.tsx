86db6674e67425393d798befb39395a48c6d7615
Joffre Veloz <joffreandres11@gmail.com>
2024-01-13 19:50:30 -0500

    Update data-sensor.tsx

--- app/admin/components/data-sensor.tsx
+++ app/admin/components/data-sensor.tsx
@@ -188,7 +188,7 @@ export function CardsDataTableSensor() {
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="outline" className="ml-auto">
-                Columns <ChevronDownIcon className="ml-2 h-4 w-4" />
+                Columnas <ChevronDownIcon className="ml-2 h-4 w-4" />
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
